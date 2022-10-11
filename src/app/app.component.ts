7c84456bb00101e1007732503d852eea30bb1b87
jrestan <javier.restan@outlook.es>
2022-10-11 00:02:32 -0500

    actualizacion casa 10-10-2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+
+  mensaje: string = "Javier"
+
+  onCambiarNombre(){
+    this.mensaje = "Javier Restan Zegarra";
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
+
+// //PrimeNg
+// import {ButtonModule} from 'primeng/button';
+// import {CardModule} from 'primeng/card';
+
+//ahora las importaciones de primefaces esta en mi modulo personalizado
+//import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+
+    //ahora las importaciones de primefaces esta en mi modulo personalizado
+    //PrimeNgModule
+    // ButtonModule,
+    // CardModule
+
+    AppRouterModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
